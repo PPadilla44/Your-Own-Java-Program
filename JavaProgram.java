d6594458029df9bdda4762fe458c330ed06d4063
Pablo Padilla <Padilla.Pablo1998@gmail.com>
2021-06-04 14:33:19 -0700

    inital commit

---
+++ JavaProgram.java
@@ -0,0 +1,7 @@
+public class JavaProgram{
+    public static void main(String[] args){
+        System.out.println("My name is Pablo Padilla");
+        System.out.println("I am 23 years old");
+        System.out.println("My hometown is South San Francisco, CA");
+    }
+}
